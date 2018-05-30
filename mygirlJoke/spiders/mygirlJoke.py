01219a841f38385cdae24cd37cbdb38f02430694
JackDan9 <1835812864@qq.com>
2018-05-30 23:08:43 +0800

    :cn: mygirlJoke : scrapy : python

--- mygirlJoke/spiders/mygirlJoke.py
+++ mygirlJoke/spiders/mygirlJoke.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
+import smtplib
+import datetime
+from email.mime.text import MIMEText
+from email.header import Header
 from scrapy.http import Request
-import os
 
 class MygirlJokeSpider(scrapy.Spider):
     name="mygirlJoke"
@@ -25,11 +28,52 @@ class MygirlJokeSpider(scrapy.Spider):
         # print(New_Joke_Url
         yield Request(
             url=New_Joke_Url,
+            # url="https://www.qiushibaike.com/article/110329808",
             callback=self.parse_content
         )
 
     def parse_content(self, response):
         Laugh_Selector = response.xpath('.//div[@class="article block untagged noline mb15"]')
-        Laugh_Content = Laugh_Selector.xpath('.//div[@class="content"]/text()').extract()[0]
-        print(Laugh_Content)
-
+        Laugh_Content_List = Laugh_Selector.xpath('.//div[@class="content"]/text()').extract()
+        Laugh_Img_Pic = Laugh_Selector.xpath('.//div[@class="thumb"]/img/@src').extract()[0] or ''
+        Laugh_Img = "https:" + str(Laugh_Img_Pic)
+        for Laugh_Content_Single in Laugh_Content_List:
+            Laugh_Content = ''.join(Laugh_Content_Single).encode('utf-8')
+        today = datetime.datetime.today()
+        anniversary = datetime.datetime(2018, 3, 14)
+        loving_days = (today - anniversary).days
+        print(type(Laugh_Img))
+        if (Laugh_Img_Pic and len(Laugh_Img_Pic)!= 0):
+            lst = [
+                '<html><body><h3>你好, 呆瓜:<br><br></h3>' + 
+                '<h4>今天是' +  today.strftime('%Y-%m-%d') +':<br></h4>' + 
+                '<h4>首先，今天已经是我们相恋的第' + str(loving_days) + '天了喔。然后大兵就要为你带来欢声笑语了！！</h4>' +
+                '<h4>今日笑话内容:<br>' + Laugh_Content + '<br></h4>' +
+                '<img src=' + str(Laugh_Img) +'><br><br>' +
+                '</body></html>']
+        else:
+            lst = ['<html><body><h3>你好, 呆瓜:<br><br></h3>' + 
+                '<h4>今天是' +  today.strftime('%Y-%m-%d') +':<br></h4>' + 
+                '<h4>首先，今天已经是我们相恋的第' + str(loving_days) + '天了喔。然后大兵就要为你带来欢声笑语了！！</h4>' +
+                '<h4>今日笑话内容:<br>' + Laugh_Content + '<br></h4>' +
+                '</body></html>']
+            
+        mailto_list = "***********@qq.com"
+        mail_host = "smtp.qq.com"
+        mail_user = "***********@qq.com"
+        mail_pass = "**************"
+        
+        content = ''.join(lst)
+        msg = MIMEText(content, _subtype='html', _charset='utf-8')
+        msg['From'] = mail_user
+        msg['To'] = mailto_list
+        msg['Subject'] = Header('大兵男朋友的笑话', 'utf-8')
+        try:
+            # s = smtplib.SMTP_SSL(mail_host, 465)
+            s = smtplib.SMTP_SSL(mail_host, 465)
+            s.login(mail_user, mail_pass)
+            s.sendmail(mail_user, mailto_list, msg.as_string())
+            s.close()
+        except Exception as e:
+            # traceback.print_exc()
+            print(e)
